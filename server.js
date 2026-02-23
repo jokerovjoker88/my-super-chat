de645d483d0743eac00f602aeb76ff8dfebe5152
jokerovjoker88 <jokerovjoker88@gmail.com>
2026-02-24 01:17:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-// Работа с папкой public
 app.use(express.static(path.join(__dirname, 'public')));
 
 const db = new Client({ 
@@ -29,7 +28,6 @@ async function boot() {
 }
 boot();
 
-// Роут для фронтенда
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
@@ -63,19 +61,24 @@ io.on('connection', (socket) => {
     socket.on('search_user', async (n) => {
         const res = await db.query("SELECT username, avatar FROM users WHERE username = $1", [n]);
         if (res.rows[0]) socket.emit('user_found', res.rows[0]);
+        else socket.emit('auth_error', 'Пользователь не найден');
     });
 
     socket.on('load_chat', async (d) => {
-        const res = await db.query("SELECT sender, content, type, to_char(ts, 'HH24:MI') as time FROM messages WHERE (sender=$1 AND receiver=$2) OR (sender=$2 AND receiver=$1) ORDER BY ts ASC", [d.me, d.him]);
+        const res = await db.query("SELECT sender as from, receiver as to, content, type, to_char(ts, 'HH24:MI') as time FROM messages WHERE (sender=$1 AND receiver=$2) OR (sender=$2 AND receiver=$1) ORDER BY ts ASC", [d.me, d.him]);
         socket.emit('chat_history', res.rows);
     });
 
     socket.on('send_msg', async (d) => {
-        const res = await db.query("INSERT INTO messages (sender, receiver, content, type) VALUES ($1, $2, $3, $4) RETURNING to_char(ts, 'HH24:MI') as time", [d.from, d.to, d.content, d.type]);
-        io.to(d.to).to(d.from).emit('new_msg', { ...d, time: res.rows[0].time });
+        try {
+            const res = await db.query(
+                "INSERT INTO messages (sender, receiver, content, type) VALUES ($1, $2, $3, $4) RETURNING to_char(ts, 'HH24:MI') as time", 
+                [d.from, d.to, d.content, d.type]
+            );
+            const msgWithTime = { ...d, time: res.rows[0].time };
+            io.to(d.to).to(d.from).emit('new_msg', msgWithTime);
+        } catch (e) { console.error(e); }
     });
 });
 
-server.listen(process.env.PORT || 10000, () => {
-    console.log('Server is running on port 10000');
-});
+server.listen(process.env.PORT || 10000);
